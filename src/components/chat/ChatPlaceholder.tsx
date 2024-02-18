440c8e7d49c2d916cc9bb494b8501e91bb584e98
Zelilac <zianudon@gmail.com>
2024-02-18 23:24:16 +0700

    sec

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -9,7 +9,8 @@ export default function ChatPlaceholder({}: Props) {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="max-w-3xl p-4 text-center text-primary">
-        <h1 className="text-4xl font-medium">PDOG-GPT Playground</h1>
+        <img src="/podog-head.png" alt="Deskripsi Gambar" className="mx-auto" style={{ maxWidth: '90px' }} /> {/* Atur maxWidth sesuai kebutuhan */}
+        <h1 className="text-4xl font-medium">PDOG AI Chatbot</h1>
         <p className="mt-4 text-lg">
         PDOG chatbot will engage users, answer questions about the Solana ecosystem, and share updates on the PDOG token. It will embody the characteristics of a loyal canine companion, 
         always ready to assist and inform its human friends.
